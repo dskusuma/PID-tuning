c3c984e4dc3ce2df1ee99bab838cb312d0728702
Dwi Satria <dwisatriakusuma4@gmail.com>
2019-01-17 18:24:31 +0700

    update

--- draft_PID/sketch_jan10a/sketch_jan10a.ino
+++ draft_PID/sketch_jan10a/sketch_jan10a.ino
@@ -10,31 +10,49 @@
 //GLOBAL VARIBLES
 
 // Variable
-// omega = ...
-// zeta = ...
-// persen_ts = ... {x | x =  5% || 2%% || 0.5% }
-const double omega_n = 5;
-const double zeta = 3;
-
-
+const float pi = 3.14;
+const double Kd = 0;
+float Ts, zeta, Wn, overshoot, Kp, Ki;
 void setup() {
   // TODO Gean: 
   // yang dibutuhk{an: pin1, gnd, 5v, pin16, pin17, gnd2
+  //  Manual input
+  overshoot = 12.8;
+  Ts = 0.0216;
   
+  Serial.begin(115200);
 }
 
 void loop() {
-  // Analogi:
-  //
-  // Ketika ada nilai di pin1: 
-  //            -> nilai ini masuk ke Tp 
-  //            -> masuk ke persamaan Tp
-  //            -> kita dapat nilai Tp
 
-  // Masuk ke persaman ts(0.5%) menggunakan nilai omega dan zeta -> dapat nilai Ts
   
-  // Masuk ke Mp 
-  
+//  if(Serial.available() > 0) {
+    zeta = hitungZeta(overshoot);
+    Wn = hitungWn(Ts, zeta);
+    Kp = hitungKp(zeta, Wn);
+    Ki = hitungKi(Wn);
+
+    Serial.print("Wn: ");
+    Serial.print(Wn, DEC);
+
+    Serial.print("| zeta: ");
+    Serial.print(zeta, DEC);
+    Serial.println("");
+    
+    Serial.print("Kp: ");
+    Serial.print(Kp, DEC);
+
+    Serial.print("| Ki: ");
+    Serial.print(Ki, DEC);
+
+    Serial.print("| Kd: ");
+    Serial.print(Kd, DEC);
+
+    Serial.println("");
+
+    Serial.print("===================");
+    Serial.println("");
+//  }
 }
 
 
@@ -67,4 +85,35 @@ float hitung_Ts(float persen, float zeta, float omega_n) {
   }
 
   Ts = var_ts / (zeta * omega_n);
+}
+
+// ================================
+//  2.FUNGSI BARU
+// ================================
+
+// Cari Zeta
+float hitungZeta(float overshoot) {
+    float zeta;
+    zeta = sqrt(1 / (pi + log(overshoot/100) ) );
+    return zeta;
+}
+
+// Cari Wn
+float hitungWn(float Ts, float zeta) {
+  float Wn;
+  Wn = 3 / (zeta * Ts);
+  return Wn;
+}
+
+// Cari Kp
+float hitungKp(float zeta, float Wn) {
+  float Kp;
+  Kp = (2 * zeta * Wn - 3.894) / 712.6;
+  return Kp;
+}
+
+float hitungKi(float Wn) {
+  float Ki;
+  Ki = (pow(Wn,2) / 712.6);
+  return Ki;
 }
